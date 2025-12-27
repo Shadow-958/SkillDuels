88fecce657534f7a0f5aae55126b29f51484d07d
aayus <aayus@Acer_Aspire.(none)>
2025-12-27 21:02:21 +0530

    first commit

--- backend/routes/leaderboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getGlobalLeaderboard, getWeeklyLeaderboard } = require("../controllers/leaderboardController");
-
-router.get("/global", getGlobalLeaderboard);
-router.get("/weekly", getWeeklyLeaderboard);
-
-module.exports = router;
